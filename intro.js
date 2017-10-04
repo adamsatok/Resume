6768752950d4e7a13bd3899a796c5234ab12804b
Adam Satok <adamsatok@gmail.com>
2017-10-04 00:32:39 -0400

    Finished first draft of front page

--- intro.js
+++ intro.js
@@ -1,10 +1,9 @@
 var delaylen = 500;
 var spreadlen = 500;
-$(document).ready(function(){
-$("#btn1").click(function(){
+function animateIntro(){
 		$(".animatedBox").animate({height: "150px"}, delaylen);
-        $(".thickBorder").delay(delaylen).animate({width: "580px"}, spreadlen);
+        $(".thickBorder").delay(delaylen).animate({width: "580px",borderWidth: "0"}, spreadlen);
         $(".animatedBox").animate({width: "580px"}, spreadlen); 
         $(".introSubtext").delay(delaylen+spreadlen).animate({opacity: "1"}, spreadlen); 
-    });
-});
+    }
+ window.onload = function(){animateIntro();};
